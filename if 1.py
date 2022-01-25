a9faecb9217b714e94506427dd18521dd5a630e3
MUNIA BHARATHI <97044198+MuniaBharathi@users.noreply.github.com>
2022-01-25 17:32:26 +0530

    Add files via upload

---
+++ if 1.py
@@ -0,0 +1,16 @@
+#If...else stamements
+
+#1
+snow=0
+if snow==1:
+   print("I will go out")
+else:
+    print("I will not")
+
+#2
+rain=1
+if rain==1:
+   print("I will dance")
+else:
+   print("I will not")
+   
