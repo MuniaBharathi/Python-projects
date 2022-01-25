615d23e0fabdbd8a07c061447e79bc7c6c40c7e2
MuniaBharathi <97044198+MuniaBharathi@users.noreply.github.com>
2022-01-25 11:38:14 +0530

    Update area.py

--- area.py
+++ area.py
@@ -1,5 +1,6 @@
 #Python program to print area of the circle
 pi=3.142
-r=float(input("Enter the radius of the circle r"))
+r=float(input("Enter the radius of the circle r :"))
 a=pi*r*r
 print("Area of the circle = ",a)
+
