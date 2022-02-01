b88f97e05380b2cd551d78b46057e53608a44666
MUNIA BHARATHI <97044198+MuniaBharathi@users.noreply.github.com>
2022-02-01 07:46:50 +0530

    Update fibbonoic sequence.py

--- fibbonoic sequence.py
+++ fibbonoic sequence.py
@@ -1,5 +1,13 @@
-a,b=0,1
-while b>=0:
-    print(b)
-    a,b=b,b+a
+#Fibbonic series
+nums=int(input("Enter any number :"))
+n1,n2=0,1
+sum=0
+if nums<=0:
+    print("Please enter number greater than 0")
+else:
+    for i in range(0,nums):
+        print(sum,end=" ")
+        n1=n2
+        n2=sum
+        sum=n1+n2
 
