07e94c8dc81e3d44fe7c0a963eb151225b09d3d7
MUNIA BHARATHI <97044198+MuniaBharathi@users.noreply.github.com>
2022-02-01 06:59:34 +0530

    Update area.py

--- area.py
+++ area.py
@@ -4,3 +4,8 @@ r=float(input("Enter the radius of the circle r :"))
 a=pi*r*r
 print("Area of the circle = ",a)
 
+#Extract the extension of the file
+fileName=input("Write the fileName")
+file_extns=fileName.split(".")
+print("The extension is"+repr(file_extns[-1]))
+
